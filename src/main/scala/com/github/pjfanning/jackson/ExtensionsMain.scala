8d767ce05f49ead2af1ed3f316a2f248df11de5a
PJ Fanning <pjfanning@users.noreply.github.com>
2022-01-30 00:19:27 +0100

    update

--- src/main/scala/com/github/pjfanning/jackson/ExtensionsMain.scala
+++ src/main/scala/com/github/pjfanning/jackson/ExtensionsMain.scala
@@ -7,7 +7,10 @@ import com.github.pjfanning.jackson.reflection.ScalaReflectionExtensions
 object ExtensionsMain extends App {
   //create mapper with ScalaReflectionExtensions mixin
   val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build() :: ScalaReflectionExtensions
-  //deserialization with Option[Long] usually fails but succeeds with this mixin
+  //deserialization with Option[Long] usually generates an invalid Option[Long] but succeeds with this mixin
+  //see https://github.com/FasterXML/jackson-module-scala/wiki/FAQ#deserializing-optionint-seqint-and-other-primitive-challenges
   val v1 = mapper.readValue[WrappedOptionLong]("""{"text":"myText","wrappedLong":{"valueLong":151}}""")
   println(v1)
+  val valueLong = v1.wrappedLong.valueLong.get
+  println(s"valueLong=$valueLong")
 }
--- src/main/scala/com/github/pjfanning/jackson/RTypeMain.scala
+++ src/main/scala/com/github/pjfanning/jackson/RTypeMain.scala
@@ -7,12 +7,15 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.github.pjfanning.jackson.reflection.ScalaReflectionExtensions
 
 object RTypeMain extends App {
-  // because the co.blocke/scala-reflection compiler plugin is enabled, the rtype is evaluated at compile time
+  //because the co.blocke/scala-reflection compiler plugin is enabled, the rtype is evaluated at compile time
   val rtype = RType.of[WrappedOptionLong]
-  // this ScalaReflectionExtensions call will configure jackson-module-scala with the correct type info for WrappedOptionLong
+  //this ScalaReflectionExtensions call will configure jackson-module-scala with the correct type info for WrappedOptionLong
   ScalaReflectionExtensions.registerInnerTypes(rtype)
   val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
-  // the following readValue would fail without the registerInnerTypes call above
+  //the following readValue would create an invalid Option[Long] without the registerInnerTypes call above
+  //see https://github.com/FasterXML/jackson-module-scala/wiki/FAQ#deserializing-optionint-seqint-and-other-primitive-challenges
   val v1 = mapper.readValue("""{"text":"myText","wrappedLong":{"valueLong":151}}""", classOf[WrappedOptionLong])
   println(v1)
+  val valueLong = v1.wrappedLong.valueLong.get
+  println(s"valueLong=$valueLong")
 }
